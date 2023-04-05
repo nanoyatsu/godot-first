0adbcf3578ff6c43369f5572f5cd10bb61150ea9
nanoyatsu <34255833+nanoyatsu@users.noreply.github.com>
2023-04-06 01:38:09 +0900

    操作できるようになった

--- GodotFirstFs/CircleFs.fs
+++ GodotFirstFs/CircleFs.fs
@@ -9,6 +9,15 @@ type CircleFs() =
     let angularSpeed = Mathf.Pi
 
     override this._Process delta =
-        this.Rotation <- this.Rotation + (angularSpeed * (float32 delta))
+        let direction =
+            if Input.IsActionPressed "ui_left" then -1
+            elif Input.IsActionPressed "ui_right" then 1
+            else 0
+
+        this.Rotation <- this.Rotation + (angularSpeed * (float32 delta)) * float32 direction
         let velocity = (Vector2.Up.Rotated this.Rotation) * (float32 speed)
-        this.Position <- this.Position + (velocity * (float32 delta))
+
+        if Input.IsActionPressed "ui_up" then
+            this.Position <- this.Position + (velocity * (float32 delta))
+        elif Input.IsActionPressed "ui_down" then
+            this.Position <- this.Position - (velocity * (float32 delta))
