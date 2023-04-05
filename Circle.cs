d684cb5f87be10261dbbd6dcd95926f65f90c733
nanoyatsu <34255833+nanoyatsu@users.noreply.github.com>
2023-04-06 01:18:38 +0900

    コミット漏れとる

---
+++ Circle.cs
@@ -0,0 +1,19 @@
+using GodotFirstFs;
+
+public partial class Circle : CircleFs
+{
+    public override void _Ready()
+    {
+        base._Ready();
+    }
+
+    public override void _Draw()
+    {
+        base._Draw();
+    }
+
+    public override void _Process(double delta)
+    {
+        base._Process(delta);
+    }
+}
