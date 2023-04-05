3322b28d77652a4a94ab027d05efd3b37eaa62a3
nanoyatsu <34255833+nanoyatsu@users.noreply.github.com>
2023-04-06 01:15:22 +0900

    ぐるぐる回るようになった

---
+++ GodotFirstFs/CircleFs.fs
@@ -0,0 +1,14 @@
+﻿namespace GodotFirstFs
+
+open Godot
+
+type CircleFs() =
+    inherit Sprite2D()
+
+    let speed = 400
+    let angularSpeed = Mathf.Pi
+
+    override this._Process delta =
+        this.Rotation <- this.Rotation + (angularSpeed * (float32 delta))
+        let velocity = (Vector2.Up.Rotated this.Rotation) * (float32 speed)
+        this.Position <- this.Position + (velocity * (float32 delta))
